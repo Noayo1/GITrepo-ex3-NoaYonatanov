33aba2cfd17618494f9c6c52ec1a7939fb4da817
Noayo1 <nyo1254@gmail.com>
2024-12-08 19:46:40 +0200

    Initial change commit

--- NoaYonatanov.java
+++ NoaYonatanov.java
@@ -1,7 +1,7 @@
 public class NoaYonatanov{
 	public static void main(String[] args){
 		for(int i=1;i<=10;i++){
-				System.out.println("NoaYonatanov..."+i);
+				System.out.println("Update NoaYonatanov"+i);
 		}
 	}
 }
